f7f6ff7f8bbbb674153f8aae917c9644a03b3dbb
Quentin Agren <quentin.agren@sciencespo.fr>
2018-12-17 05:50:54 +0100

    Day 10 level 1

--- day10.py
+++ day10.py
@@ -20,10 +20,10 @@ def parse_input(filename=INPUT_FILENAME):
             vyy.append(vy)
     return dict(x=xx, y=yy, vx=vxx, vy=vyy)
 
-def step(coords):
+def step(coords, n_steps=1):
     for i in range(len(coords['x'])):
-        coords['x'][i] += coords['vx'][i]
-        coords['y'][i] += coords['vy'][i]
+        coords['x'][i] += n_steps * coords['vx'][i]
+        coords['y'][i] += n_steps * coords['vy'][i]
     return coords
 
 def get_grid_dimensions(coords):
@@ -33,51 +33,30 @@ def get_grid_dimensions(coords):
     my, My = min(yy), max(yy)
     return (mx, my, Mx - mx, My - my)
 
-def scaled(coords, width=100, height=60):
-    x0, y0, w, h = get_grid_dimensions(coords)
-    s_x = float(width) / w
-    s_y = float(height) / h
-    s_coords = {}
-    s_coords['x'] = [(x - x0) * s_x for x in coords['x']]
-    s_coords['vx'] = [ vx * s_x for vx in coords['vx']]
-    s_coords['y'] = [(y - y0) * s_y for y in coords['y']]
-    s_coords['vy'] = [ vy * s_y for vy in coords['vy']]
-    return s_coords
-
-def std(coords):
-    mx = sum(coords['x'])/float(len(coords['x']))
-    my = sum(coords['y'])/float(len(coords['y']))
-    sx = sum((x - mx)**2 for x in coords['x'])
-    sy = sum((y - my)**2 for y in coords['y'])
-    return sx + sy
-
-    
-def draw_grid(coords, width=100, height=50):
-    grid = [['.' for _ in range(width + 1)] for _ in range(height + 2)]
-    for (x, y) in zip(coords['x'], coords['y']):
-        grid[round(x)][round(y)] = '*'
-    print('#' * width)
-    for line in grid:
-        print(''.join(line))
-
 import sys
 import math
-from random import random
 def animate_grid(stdscr):
+    y, x = stdscr.getmaxyx()
+    height, width = y - 5, x - 5
     coords = parse_input()
     stdscr.clear()
-    prev_dims = math.inf
-    idx = 1
+    prev_h = prev_w = math.inf
+    idx = 9000
+    step(coords, n_steps=idx)
     while(1):
-        s_coords = scaled(coords)
-        _,_,h,w = get_grid_dimensions(coords)
-        if h + w >= prev_dims:
+        x0,y0,w,h = get_grid_dimensions(coords)
+        if w >= prev_w or w >= prev_w:
             break
-        prev_dims = h + w 
-        grid = [['.' for _ in range(101)] for _ in range(61)]
-        for (x, y) in zip(s_coords['x'], s_coords['y']):
-            grid[round(y)][round(x)] = '*'
-        stdscr.addstr(0,0,f'{prev_dims} it={idx}')
+        prev_h, prev_w = h, w
+        sw = min(width, w)
+        sh = min(height, h)
+        grid = [['.' for _ in range(sw + 1)] for _ in range(sh + 1)]
+        for (x, y) in zip(coords['x'], coords['y']):
+            j = round((y - y0) * sh / h)
+            i = round((x - x0) * sw / w)
+            grid[j][i] = '*'
+        stdscr.clear()
+        stdscr.addstr(0,0,f'iteration: {idx} width: {w} height: {h} scrren w: {sw} screen h: {sh}        ')
         for (j, line) in enumerate(grid):
             stdscr.addstr(j + 1, 0, ''.join(line))
         stdscr.refresh()
@@ -86,7 +65,5 @@ def animate_grid(stdscr):
     sys.stdin.read()
 
 if __name__ == '__main__':
-    from curses import wrapper
-    wrapper(animate_grid)
-
-    # 10408611
+    import curses
+    curses.wrapper(animate_grid)
